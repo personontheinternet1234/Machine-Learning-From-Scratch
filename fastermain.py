31dbb4eec01e1c8f891e71a8594bbe1031c3f156
githubCHM <143050709+githubCHM@users.noreply.github.com>
2023-10-02 08:56:34 -1000

    Update fastermain.py
    changed fastermain.py

--- fastermain.py
+++ fastermain.py
@@ -2,9 +2,27 @@ import numpy as np
 import random
 from tqdm import tqdm
 
-actual_outputs = ["0", "1"]
+# class NonLinearity:
+    
+#     def __init__(self, values):
+#         self.values = values
+        
+#     class sigmoid(self, values):
+#         self.values = values
 
-# float_possibilities = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "-"]
+#         define forward(self):
+#             output = 1 / (1 + np.exp(-1 * values))
+#             return output
+
+#         define prime(self):
+#             output = 1 / (1 + np.exp(-1 * values)) * (1 - 1 / (1 + np.exp(-1 * values)))
+#             return output
+        
+        
+
+
+# class Error:
+#     ...
 
 
 def sigmoid(values):
@@ -17,14 +35,18 @@ def sigmoid_prime(values):
     return output
 
 
-def relu(x):  # Leaky Relu function, with a slope of 0.1 for neg values built for use with matrices
-    return np.maximum(0.1 * x, x)
+def relu(values):
+    output = np.maximum(0.1 * values, values)
+    return output
 
 
-def derivative_relu(scalar_passed_to_relu):  # derivative of our leaky relu
-    return 1 if (scalar_passed_to_relu > 0) else 0.1
+def relu_prime(values):
+    if values > 0:
+        return 1
+    else:
+        return 0.1
 
-
+# training data
 input_training = [
     [0, 0],
     [1, 0],
@@ -38,31 +60,36 @@ output_training = [
     [0, 0]
 ]
 
+# user output index
+user_outputs = ["0", "1"]
+
 # add hidden layers when necessary
 input_size = 2
-hidden_layers = 2  # update for this
-hidden_sizes = [3, 2]  # update for this
+hidden_layers = 2  # update for this later
+hidden_sizes = [3, 2]  # update for this later
 hidden_1_size = 3
 output_size = 2
-epochs = 10000
+epochs = 3
 learning_rate = 0.001
 
+# instantiate weights and biases
 w0 = np.random.randn(hidden_1_size, input_size)
 b1 = np.zeros((hidden_1_size, 1))
-w1 = np.random.randn(output_size, hidden_1_size)  # going_to x from
+w1 = np.random.randn(output_size, hidden_1_size)
 b2 = np.zeros((output_size, 1))
 
+# training loop
 for i in range(epochs):
 
-    # forward pass
+    # forward pass # this works i think
 
     # choose from training set
     training_choice = random.randint(0, len(input_training) - 1)
 
     # reformat inputs and outputs
-    a0 = np.reshape(input_training[training_choice], (len(input_training[training_choice]), 1))
-    c = np.reshape(output_training[training_choice], (len(output_training[training_choice]), 1))
-
+    a0 = np.reshape(np.array(input_training[training_choice]), (len(input_training[training_choice]), 1))
+    c = np.reshape(np.array(output_training[training_choice]), (len(output_training[training_choice]), 1))
+    
     # calculate outputs
     a1 = sigmoid(np.matmul(w0, a0) + b1)
     a2 = sigmoid(np.matmul(w1, a1) + b2)
@@ -74,39 +101,33 @@ for i in range(epochs):
     d_b2 = sigmoid_prime(d_a2)
     d_w1 = np.multiply(np.resize(d_b2, (hidden_1_size, output_size)).T, np.resize(a1.T, (output_size, hidden_1_size)))
 
-    # pseudo
-    # [
-    #     [1, 2, 4.5]
-    #     [4.5, 2, 3]
-    # ]
-    #
-    # [
-    #     [1]
-    #     [2]
-    # ]
-    # g_activation = connection.weight * g_upstream[connectionindex][0]
-
-    # d_a1_0 = np.sum(np.multiply(w1[0], d_b2))
-
     # first layer
-    d_a1 = np.reshape(np.sum(np.multiply(w1, d_b2), axis=0), (len(a1), 1))
+    d_a1 = np.reshape(np.sum(np.multiply(np.resize(d_b2, (hidden_1_size, output_size)), w1.T), axis=1), (hidden_1_size, 1))
     d_b1 = sigmoid_prime(d_a1)
     d_w0 = np.multiply(np.resize(d_b1, (input_size, hidden_1_size)).T, np.resize(a0.T, (hidden_1_size, input_size)))
 
     # optimize weights and biases
 
     w0 = np.subtract(w0, learning_rate * d_w0)
+    print(d_w0)
+    print(w0)
     b1 = np.subtract(b1, learning_rate * d_b1)
+    print(d_b1)
+    print(b1)
     w1 = np.subtract(w1, learning_rate * d_w1)
+    print(d_w1)
+    print(w1)
     b2 = np.subtract(b2, learning_rate * d_b2)
+    print(d_b2)
+    print(b2)
 
-    # error report
+    # error report every few epochs
 
-    if i % 1000 == 0:
+    if i % 1 == 0:
         error = 0
         for j in range(len(a2)):
             error += (c[j] - a2[j]) ** 2
-        print("SSR: " + str(error))
+        print(f"Error: {error} ({round(i / epochs * 100)}%)")
 
 # return optimized weights and biases
 print("")
@@ -119,20 +140,20 @@ print(w1)
 print("b2:")
 print(b2)
 
-# allows user to plug in values
+# allow user to test optimized network
 while True:
     print("")
     inputs = []
     for i in range(input_size):
         inputs.append(float(input("a(0)" + str(i) + ": ")))  # gets inputs
 
-    # calculates
+    # forward pass
     a0 = np.reshape(inputs, (len(inputs), 1))
-    a1 = sigmoid(np.dot(w0, a0) + b1)
-    a2 = sigmoid(np.dot(w1, a1) + b2)
-
-    # prints result
+    a1 = sigmoid(np.matmul(w0, a0) + b1)
+    a2 = sigmoid(np.matmul(w1, a1) + b2)
+  
+    # result
     print("")
     print(a2)
     output_index = np.nanargmax(np.where(a2 == a2.max(), a2, np.nan))
-    print(actual_outputs[output_index])
+    print(user_outputs[output_index])
