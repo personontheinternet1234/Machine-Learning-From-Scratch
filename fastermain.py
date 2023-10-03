6e90b537b5a0f5e00cabb031f23bc9d891c3a3d0
githubCHM <143050709+githubCHM@users.noreply.github.com>
2023-10-02 18:04:36 -1000

    Update fastermain.py
    tried to move around some stuff

--- fastermain.py
+++ fastermain.py
@@ -23,12 +23,11 @@ from tqdm import tqdm
 #         define prime(self):
 #             output = 1 / (1 + np.exp(-1 * values)) * (1 - 1 / (1 + np.exp(-1 * values)))
 #             return output
-        
-        
-
 
 # class Error:
 #     ...
+
+
 
 def sigmoid(values):
     output = 1 / (1 + np.exp(-1 * values))
@@ -52,59 +51,28 @@ def relu_prime(values):
     else:
         return 0.1
 
-
-
-def forward(training_choice):
-    # forward pass # this works i think
-    global a1
-    global a2
-    global a0
-    global c
-    # reformat inputs and outputs
-    a0 = np.reshape(np.array(input_training[training_choice]), (len(input_training[training_choice]), 1))
-    c = np.reshape(np.array(output_training[training_choice]), (len(output_training[training_choice]), 1))
-    
-    # calculate outputs
+def forward_pass(a0, w0, b1, w1, b2):
+    # forward pass
     a1 = sigmoid(np.matmul(w0, a0) + b1)
     a2 = sigmoid(np.matmul(w1, a1) + b2)
-
+    
     return a2
 
-# learning algorithm
+# def backpropagate():
+    # ...    
 
-def backward():  # calculate gradients
-    global w0
-    global b1
-    global w1
-    global b2
 
-    # second layer
-    d_a2 = -2 * np.subtract(c, a2)
-    d_b2 = sigmoid_prime(np.matmul(w1, a1) + b2) * d_a2
-    d_w1 = np.multiply(np.resize(d_b2, (hidden_1_size, output_size)).T, np.resize(a1.T, (output_size, hidden_1_size)))
 
-    # first layer
-    d_a1 = np.reshape(np.sum(np.multiply(np.resize(d_b2, (hidden_1_size, output_size)), w1.T), axis=1), (hidden_1_size, 1))
-    d_b1 = sigmoid_prime(np.matmul(w0, a0) + b1) * d_a1
-    d_w0 = np.multiply(np.resize(d_b1, (input_size, hidden_1_size)).T, np.resize(a0.T, (hidden_1_size, input_size)))
-
-    # optimize weights and biases
-
-    w0 = np.subtract(w0, learning_rate * d_w0)
-    b1 = np.subtract(b1, learning_rate * d_b1)
-    w1 = np.subtract(w1, learning_rate * d_w1)
-    b2 = np.subtract(b2, learning_rate * d_b2)
-
-# main code
+# code
 
 # user output index
-user_outputs = ["0", "1"]
+user_outputs = ["1", "2"]
 
 # neural network structure
 input_size = 2
 hidden_layers = 2  # update for this later
 hidden_sizes = [3, 2]  # update for this later
-hidden_1_size = 3  # write this out
+hidden_1_size = 3  # phase this out
 output_size = 2
 
 # learning presets
@@ -121,10 +89,10 @@ input_training = [
     [1, 1]
 ]
 output_training = [
-    [0, 0],
-    [0, 1],
+    [1, 1],
     [1, 0],
-    [0.5, 0.5]
+    [0, 1],
+    [0, 0]
 ]
 
 # instantiate weights and biases
@@ -134,24 +102,44 @@ w1 = np.random.randn(output_size, hidden_1_size)
 b2 = np.zeros((output_size, 1))
 
 # training loop
-
 for i in range(epochs):
     # choose from training set
     training_choice = random.randint(0, len(input_training) - 1)
     
-    forward(training_choice)
-    backward()
+    # reformat inputs and outputs
+    a0 = np.reshape(np.array(input_training[training_choice]), (len(input_training[training_choice]), 1))
+    c = np.reshape(np.array(output_training[training_choice]), (len(output_training[training_choice]), 1))
+    
+    # calculate outputs
+    a1 = sigmoid(np.matmul(w0, a0) + b1)
+    a2 = sigmoid(np.matmul(w1, a1) + b2)
 
-    # MSE  # broken
+    # calculate gradients
+
+    # second layer
+    d_a2 = -2 * np.subtract(c, a2)
+    d_b2 = sigmoid_prime(np.matmul(w1, a1) + b2) * d_a2
+    d_w1 = np.multiply(np.resize(d_b2, (hidden_1_size, output_size)).T, np.resize(a1.T, (output_size, hidden_1_size)))
+    
+    # first layer
+    d_a1 = np.reshape(np.sum(np.multiply(np.resize(d_b2, (hidden_1_size, output_size)), w1.T), axis=1), (hidden_1_size, 1))
+    d_b1 = sigmoid_prime(np.matmul(w0, a0) + b1) * d_a1
+    d_w0 = np.multiply(np.resize(d_b1, (input_size, hidden_1_size)).T, np.resize(a0.T, (hidden_1_size, input_size)))
+    
+    # optimize weights and biases
+
+    w0 = np.subtract(w0, learning_rate * d_w0)
+    b1 = np.subtract(b1, learning_rate * d_b1)
+    w1 = np.subtract(w1, learning_rate * d_w1)
+    b2 = np.subtract(b2, learning_rate * d_b2)
+
+    # error report every few epochs
+
     if i % return_rate == 0:
         error = 0
-        for point in range(len(input_training) - 1):
-            calculatedoutputs = forward(point)
-            error += (output_training[point] - calculatedoutputs) ** 2
-        error /= len(input_training)
-        error = np.sum(error)
-        print(f"MSE: {error}")
-
+        for j in range(len(a2)):
+            error += (c[j] - a2[j]) ** 2
+        print(f"Error: {error} ({round(i / epochs * 100)}%)")
 
 # return optimized weights and biases
 print("")
@@ -173,11 +161,10 @@ while True:
 
     # forward pass
     a0 = np.reshape(inputs, (len(inputs), 1))
-    a1 = sigmoid(np.matmul(w0, a0) + b1)
-    a2 = sigmoid(np.matmul(w1, a1) + b2)
+    a2 = forward_pass(a0, w0, b1, w1, b2)
   
     # result
     print("")
     print(a2)
     output_index = np.nanargmax(np.where(a2 == a2.max(), a2, np.nan))
-    print(user_outputs[output_index])
+    print(f"Predicted: {user_outputs[output_index]}")
