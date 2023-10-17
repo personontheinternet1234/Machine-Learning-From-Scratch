06e97394f7e1192f2f347029610f9cc767ef9c01
githubCHM <143050709+githubCHM@users.noreply.github.com>
2023-10-17 09:53:25 -1000

    fixed differentNNcode.py

--- Old Files/differentNNcode.py
+++ Old Files/differentNNcode.py
@@ -3,6 +3,7 @@ import random
 
 import numpy as np
 
+
 # functions
 
 def leaky_relu(inputs):
@@ -10,20 +11,24 @@ def leaky_relu(inputs):
     output = np.maximum(0.1 * inputs, inputs)
     return output
 
+
 def leaky_relu_prime(inputs):
     # leaky relu' equation
     output = np.where(inputs > 0, 1, 0.1)
     return output
 
+
 def xavier_initialize(length, width):
     # xavier initialization
-    matrix = np.random.randn(length, width) * np.sqrt(2 / length)
-    return matrix
+    array = np.random.randn(length, width) * np.sqrt(2 / length)
+    return array
+
 
 def zeros_initialize(length, width):
     # zeros initialization
-    matrix = np.zeros((length, width))
-    return matrix
+    array = np.zeros((length, width))
+    return array
+
 
 def forward_pass(inputs, weights, biases):
     # pass inputs through network
@@ -32,13 +37,14 @@ def forward_pass(inputs, weights, biases):
         activations.append(leaky_relu(np.matmul(weights[layer], activations[-1]) + biases[layer]))
     return activations
 
+
 def find_gradients(expected, activations, weights, biases):
     # initialize gradeint lists
     d_activations = []
     d_weights = []
     d_biases = []
     layers = len(activations)
-    
+
     # calculate gradients
     d_activations.insert(0, np.multiply(-2, np.subtract(expected, activations[-1])))
     for connection in range(layers - 2):
@@ -55,6 +61,7 @@ def find_gradients(expected, activations, weights, biases):
     }
     return gradients
 
+
 def optimize(initial, gradient, learning_rate):
     # adjust weights and biases
     output = []
@@ -62,20 +69,24 @@ def optimize(initial, gradient, learning_rate):
         output.append(np.subtract(initial[connection], np.multiply(learning_rate, gradient[connection])))
     return output
 
+
 def make_vector(list):
     # turn a list into a vector
     vector = np.reshape(np.array(list), (len(list), 1))
     return vector
+
 
 def argmax(inputs):
     # argmax
     index = np.nanargmax(np.where(inputs == inputs.max(), inputs, np.nan))
     return index
 
+
 def calculate_error(expected, actual):
     # SSR calculation
     error = np.sum(np.subtract(expected, actual) ** 2, axis=0)
     return error
+
 
 # network presets
 
@@ -85,9 +96,9 @@ output_index = ["checkered", "non-checkered"]
 
 # learning presets
 learn = True
-load = False
-save = False
-epochs = 10000
+load = True
+save = True
+epochs = 100000
 return_rate = 1000
 learning_rate = 0.1
 
@@ -130,32 +141,32 @@ if learn:
         for connection in range(layers - 1):
             weights.append(xavier_initialize(layer_sizes[connection + 1], layer_sizes[connection]))
             biases.append(zeros_initialize(layer_sizes[connection + 1], 1))
-    
+
     # format training data
     for data in range(len(input_training)):
         input_training[data] = make_vector(input_training[data])
     for data in range(len(output_training)):
         output_training[data] = make_vector(output_training[data])
-    
+
     # begin training loop
     for epoch in range(epochs):
         # choose from training set
-        training_choice = random.randint(0, len(input_training) - 1)  # maybe optimize?
-        
+        training_choice = random.randint(0, len(input_training) - 1)
+
         # set inputs and outputs
         activation = input_training[training_choice]
         expected = output_training[training_choice]
-        
+
         # forward pass
         activations = forward_pass(activation, weights, biases)
-        
+
         # calculate gradients
         gradients = find_gradients(expected, activations, weights, biases)
-        
+
         # optimize weights and biases
         weights = optimize(weights, gradients["d_weights"], learning_rate)
         biases = optimize(biases, gradients["d_biases"], learning_rate)
-        
+
         # error report
         if epoch % return_rate == 0:
             error = 0
@@ -164,7 +175,7 @@ if learn:
                 actual_values = forward_pass(input_training[test_case], weights, biases)[-1]
                 error += calculate_error(expected, actual_values)
             print(f"({round((epoch / epochs) * 100)}%) MSE: {error[0] / len(input_training)}")
-    
+
     # print final error
     error = 0
     for test_case in range(len(input_training)):
@@ -207,11 +218,11 @@ while True:
     inputs = []
     for node in range(layer_sizes[0]):
         inputs.append(float(input(f"{input_index[node]}: ")))
-    
+
     # forward pass
     inputs = make_vector(inputs)
     results = (forward_pass(inputs, weights, biases))[-1]
-    
+
     # return result
     print("")
     print(results)
