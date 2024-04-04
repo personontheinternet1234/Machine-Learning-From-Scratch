2c5851edea0229aba229f3ab1deec7900eccc811
IsaacVerbrugge25 <113949055+IsaacVerbrugge25@users.noreply.github.com>
2024-04-04 09:04:40 -1000

    replaced usages for backprop to chost backprop

--- Finalized_Main.py
+++ Finalized_Main.py
@@ -98,46 +98,7 @@ def forward(inputs, weights, biases):
 
 
 # backpropagation
-def backward(activations, predicted, weights, biases):
-    # initialize lists
-    d_activations = []
-    d_weights = []
-    d_biases = []
-
-    # error with respect to last layer
-    d_activations.insert(0, -2 * np.subtract(predicted, activations[-1]))
-
-    # loop through layers backwards
-    for layer in range(layers - 2, -1, -1):
-        # gradient of biases
-        d_b = d_l_relu(np.matmul(weights[layer], activations[layer]) + biases[layer]) * d_activations[0]
-        d_biases.insert(0, d_b)
-
-        # gradient of weights
-        upstream = np.resize(d_biases[0], (len(activations[layer]), len(activations[layer + 1]))).T
-        local = np.resize(activations[layer].T, (len(activations[layer + 1]), len(activations[layer])))
-
-        d_w = np.multiply(upstream, local)
-        d_weights.insert(0, d_w)
-
-        # gradient of activations
-        upstream = np.resize(d_biases[0], (len(activations[layer]), len(activations[layer + 1])))
-        totals = np.sum(np.multiply(upstream, weights[layer].T), axis=1)
-
-        d_a = np.reshape(totals, (len(activations[layer]), 1))
-        d_activations.insert(0, d_a)
-
-    for layer in range(layers - 2, -1, -1):
-        # weights[layer] = np.subtract(weights[layer], learning_rate * d_weights[layer])
-        weights[layer] = np.subtract(weights[layer],
-                                     learning_rate * (d_weights[layer] + (lambda_reg / train_len) * weights[layer]))
-        biases[layer] = np.subtract(biases[layer], learning_rate * d_biases[layer])
-
-    # return activations, weights, biases
-    return weights, biases
-
-
-def backward_2(nodes, expected, weights, biases):
+def backward(nodes, expected, weights, biases):
     # initialize gradient lists
     d_nodes = []
     d_weights = []
@@ -268,7 +229,7 @@ if learn:
         nodes = forward(inputs, weights, biases)
 
         # backpropagation
-        weights, biases = backward_2(nodes, expected, weights, biases)
+        weights, biases = backward(nodes, expected, weights, biases)
 
         # loss calculation
         if epoch % log_rate == 0:
