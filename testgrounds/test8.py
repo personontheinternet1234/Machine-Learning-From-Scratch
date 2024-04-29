dc07cec47418aa331092b6c1055ef3fbfbb9f9a3
githubCHM <143050709+githubCHM@users.noreply.github.com>
2024-04-29 12:52:13 -1000

    Update test8.py
    got non-sgd format to work

--- testgrounds/test8.py
+++ testgrounds/test8.py
@@ -36,7 +36,7 @@ il = []
 testa = []
 
 # f
-for i in range(1):
+for i in range(1000):
     # f a
     A0 = X
     A1 = l_relu(np.matmul(A0, W0) + B0)
@@ -45,50 +45,20 @@ for i in range(1):
 
     # b a
     # l2
-    dA2ao = -2 * (E - A2)
-    dB1ao = dA2ao
-    dW1ao = np.reshape(A1, (4, 3, 1)) * dB1ao
+    dA2 = -2 * (E - A2)
+    dB1 = dA2
+    dW1 = np.reshape(A1, (4, 3, 1)) * dB1
 
     # l1
-    dA1ao = np.array([np.sum(W1 * dB1ao, axis=2)])
-    dB0ao = dA1ao
-    print(np.shape(dA1ao))
-    dW0ao = np.reshape(A0, (4, 2, 1)) * dB0ao
+    dA1 = np.reshape(np.array([np.sum(W1 * dB1, axis=2)]), (4, 1, 3))
+    dB0 = dA1
+    dW0 = np.reshape(A0, (4, 2, 1)) * dB0
 
-    dW0a = 0
-    dB0a = 0
-    dW1a = 0
-    dB1a = 0
-    for tc in range(len(X)):
-        # f
-        A0_s = X[tc]
-        A1_s = l_relu(np.matmul(A0_s, W0) + B0)
-        A2_s = l_relu(np.matmul(A1_s, W1) + B1)
-        E = Y[tc]
-
-        # b
-        # l2
-        dA2 = -2 * (E - A2_s)
-        dB1 = dA2
-        dW1 = A1_s.T * dB1
-        # l1
-        dA1 = np.array([np.sum(W1 * dB1, axis=1)])
-        dB0 = dA1
-        dW0 = A0_s.T * dB0
-        # testa.append(A0_s.T)
-
-        dW0a += dW0
-        dB0a += dB0
-        dW1a += dW1
-        dB1a += dB1
-
-    # print(np.shape(np.array(testa)))
-
-    # optimize
-    W0 = W0 - lr * dW0a / len(X)
-    B0 = B0 - lr * dB0a / len(X)
-    W1 = W1 - lr * dW1a / len(X)
-    B1 = B1 - lr * dB1a / len(X)
+    # o
+    W0 = W0 - lr * np.sum(dW0, axis=0) / len(X)
+    B0 = B0 - lr * np.sum(dB0, axis=0) / len(X)
+    W1 = W1 - lr * np.sum(dW1, axis=0) / len(X)
+    B1 = B1 - lr * np.sum(dB1, axis=0) / len(X)
 
     # e
     X1 = l_relu(np.matmul(X, W0) + B0)
@@ -104,3 +74,18 @@ plt.xlabel("Epoch")
 plt.ylabel("Loss")
 plt.title("Loss v.s. Epoch")
 plt.show()
+
+while True:
+    print("")
+    inputs = []
+    for i in range(ins):
+        inputs.append(float(input(f"{str(i)}: ")))
+
+    # f
+    A0_s = np.array(inputs)
+    A1_s = l_relu(np.matmul(A0_s, W0) + B0)
+    A2_s = l_relu(np.matmul(A1_s, W1) + B1)
+
+    # r
+    print("")
+    print(A2_s)
