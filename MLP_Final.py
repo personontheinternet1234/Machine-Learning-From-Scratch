9266f201708d6255231a7e5549025918be900be5
IsaacVerbrugge25 <personontheinternet1234@gmail.com>
2024-05-22 16:02:49 -1000

    some work on class

--- MLP_Final.py
+++ MLP_Final.py
@@ -41,22 +41,46 @@ activations = {
 
 
 class MLP:
-    def __init__(self, weights, biases, activation, hidden_layer_sizes):
+    def __init__(self, weights, biases, layer_sizes):
         self.weights = weights
         self.biases = biases
-        self.hidden_layer_sizes = hidden_layer_sizes
-        self.activation = activation
+        self.layer_sizes = layer_sizes
+        self.nodes = []
 
     def _get_activation(self):
         ...
 
     def forward(self, inputs):
-        ...
+        working_nodes = [inputs]
+        for layer in range(len(self.layer_sizes) - 1):
+            activations = l_relu(np.matmul(working_nodes[-1], self.weights[layer]) + self.biases[layer])  # consider renaming
+            working_nodes.append(activations)
+        self.nodes = working_nodes
 
-    def backward(self):
-        ...
+    def sgd_backward(self, nodes, expected, weights, biases, solver, learning_rate, alpha):
+        # initialize gradient lists
+        d_weights = []
+        d_biases = []
 
-    def fit(self, solver, max_iter, learning_rate, alpha, momentum, batch_size):
+        d_b = -2 * (expected - nodes[-1])
+        d_biases.insert(0, d_b)
+        for layer in range(-1, -len(nodes) + 1, -1):
+            d_w = nodes[layer - 1].T * d_b
+            d_weights.insert(0, d_w)
+            d_b = np.array([np.sum(weights[layer] * d_b, axis=1)])
+            d_biases.insert(0, d_b)
+        d_w = nodes[0].T * d_b
+        d_weights.insert(0, d_w)
+
+        for layer in range(len(nodes) - 1):
+            weights[layer] -= learning_rate * (d_weights[layer] + (alpha / train_len) * weights[layer])
+            biases[layer] -= learning_rate * d_biases[layer]
+
+        return weights, biases
+
+    def fit(self, data_samples_and_answers, max_iter, solver, learning_rate, alpha, momentum, batch_size=200):
+        train_len = len(data_samples_and_answers)
+
         if solver == "Mini-batch":
             ...
         elif solver == "Stochastic":
