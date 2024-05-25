3bc09a6f44ba745c44f6a6a91f4940ca3a583564
githubCHM <143050709+githubCHM@users.noreply.github.com>
2024-05-24 14:52:36 -1000

    Update MLP_Final.py
    lord what is happening

--- MLP_Final.py
+++ MLP_Final.py
@@ -10,42 +10,30 @@ from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from tqdm import tqdm
 
-from PIL import Image
-
-keras_weights = []
-keras_biases = []
-with open(f"saved/weights_keras.txt", "r") as f:
-    for line in f:
-        keras_weights.append(np.array(ast.literal_eval(line)))
-with open(f"saved/biases_keras.txt", "r") as f:
-    for line in f:
-        keras_biases.append(np.array(ast.literal_eval(line)))
-
 
 class MLP:
     def __init__(self, load=False, weights="none", biases="none", layer_sizes=[784, 100, 10], activation="ReLU"):
         self.version = "1.6"
         self.load = load
-        self.weights, self.biases = self._get_parameters(weights, biases, layer_sizes)
+        self.weights, self.biases = self._get_parameters(weights, biases)
         self.layer_sizes = layer_sizes
         self.activation = self._get_activation(activation)
         self.graph_results = False
 
     def _get_parameters(self, weights, biases):
         if weights == "none":
-            weights=[]
+            weights = []
             for i in range(len(self.layer_sizes) - 1):
                 weights.append(self.xavier_initialize(self.layer_sizes[i], self.layer_sizes[i + 1]))
         else:
             weights = weights
         if biases == "none":
-            biases = biases
-        else:
-            biases=[]
+            biases = []
             for i in range(len(self.layer_sizes) - 1):
                 biases.append(np.zeros((1, self.layer_sizes[i + 1])))
+        else:
+            biases = biases
         return weights, biases
-
 
     def _get_activation(self, name):
         if name == "Sigmoid":
@@ -56,7 +44,7 @@ class MLP:
         elif name == "Tanh":
             return {
                 "forward": lambda x: np.tanh(x),
-                "derivative": lambda x: (np.cosh(x)) ** -2
+                "derivative": lambda x: 1 - np.tanh(x) ** 2
             }
         elif name == "ReLU":
             return {
@@ -69,13 +57,32 @@ class MLP:
                 "derivative": lambda x: np.where(x > 0, 1, 0.1)
             }
         else:
-            raise ValueError(f"'{name}' is an invalid activation function.")
+            raise ValueError(f'"{name}" is an invalid activation function.')
 
     def _get_solver(self, name):
         if name == "SGD":
-            ...
+            def update(nodes, Y, weights, biases):
+                d_weights = []
+                d_biases = []
+
+                d_b = -2 * (Y - nodes[-1])
+                d_biases.insert(0, d_b)
+                for layer in range(-1, -len(nodes) + 1, -1):
+                    d_w = nodes[layer - 1].T * d_b
+                    d_weights.insert(0, d_w)
+                    d_b = np.array([np.sum(weights[layer] * d_b, axis=1)])
+                    d_biases.insert(0, d_b)
+                d_w = nodes[0].T * d_b
+                d_weights.insert(0, d_w)
+
+                for layer in range(len(nodes) - 1):
+                    weights[layer] -= learning_rate * (d_weights[layer] + (alpha / train_len) * weights[layer])
+                    biases[layer] -= learning_rate * d_biases[layer]
+
+                return weights, biases
         elif name == "Mini-Batch":
-            ...
+            def update():
+                ...
         else:
             raise ValueError(f"'{name}' is an invalid solving method.")
 
@@ -90,49 +97,46 @@ class MLP:
             nodes.append(node_layer)
         return nodes
 
+    def backward(self, values, predicted, expected):
+        ...
+
     def predict(self, inputs):
         predicted = np.nanargmax(self.forward(inputs)[-1])
         return predicted
 
-    def sgd_backward(self, values, predicted, expected):
-        # initialize gradient lists
-        d_weights = []
-        d_biases = []
-
-        d_b = -2 * (expected - self.nodes[-1])
-        d_biases.insert(0, d_b)
-        for layer in range(-1, -len(self.nodes) + 1, -1):
-            d_w = self.nodes[layer - 1].T * d_b
-            d_weights.insert(0, d_w)
-            d_b = np.array([np.sum(self.weights[layer] * d_b, axis=1)])
-            d_biases.insert(0, d_b)
-        d_w = self.nodes[0].T * d_b
-        d_weights.insert(0, d_w)
-
-        # haven't tried it yet-- this code could cause problems.
-        for layer in range(len(self.nodes) - 1):
-            self.weights[layer] -= learning_rate * (d_weights[layer] + (alpha / train_len) * self.weights[layer])
-            self.biases[layer] -= learning_rate * d_biases[layer]
-
-    def set_hyperparameters(self):
-
     def set_output_configuration(self, graph_results=False, cm_normalization="true", eval_batching=True, eval_batch_size=5, eval_interval=10):
         self.graph_results = graph_results
+        self.cm_normalization = cm_normalization
+        self.eval_batching = eval_batching
+        self.eval_batch_size = eval_batch_size
+        self.eval_interval = eval_interval
 
-    def fit(self, values, labels, solver="Mini-Batch", alpha=0.1, batch_size="auto", learning_rate=0.001, max_iter=200, momentum=0.9):
+    def fit(self, X, Y, solver="Mini-Batch", alpha=0.1, batch_size="auto", learning_rate=0.001, max_iter=200, momentum=0.9):
         self.solver = self._get_solver(solver)
-        for epoch in range(self.max_iter):
+        train_len = len(X)
+        for epoch in range(max_iter):
+            tc = random.randint(0, train_len - 1)
+            inputs = X[tc]
+            expected = Y[tc]
             nodes = self.forward()
+            self.weights, self.biases = self.backward()
 
 
-neural_net = MLP(weights=keras_weights, biases=keras_biases, layer_sizes=[784, 16, 16, 10])
-
+# loading data for testing
+from PIL import Image
+keras_weights = []
+keras_biases = []
+with open(f"saved/weights_keras.txt", "r") as f:
+    for line in f:
+        keras_weights.append(np.array(ast.literal_eval(line)))
+with open(f"saved/biases_keras.txt", "r") as f:
+    for line in f:
+        keras_biases.append(np.array(ast.literal_eval(line)))
 img = Image.open(f"saved/user_number.jpeg")
-# grayscale image
 gray_img = img.convert("L")
-# convert to numpy array
-forward_layer = np.array(list(gray_img.getdata())) / 255
+test_input = np.array(list(gray_img.getdata())) / 255
 
-print(neural_net.forward(forward_layer, activation_name="LeakyReLU")[-1])
-
-print(neural_net.predict(forward_layer))
+# class testing
+neural_net = MLP(weights=keras_weights, biases=keras_biases, layer_sizes=[784, 16, 16, 10], activation="Leaky ReLU")
+print(neural_net.forward(test_input)[-1])
+print(neural_net.predict(test_input))
