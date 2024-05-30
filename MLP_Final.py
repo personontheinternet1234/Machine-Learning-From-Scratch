d0965512b306072ec5f6ca7af784c323b44900b6
githubCHM <143050709+githubCHM@users.noreply.github.com>
2024-05-29 16:07:13 -1000

    updated MLP class
    made loss reporting (maybe) and fixed some formatting things (maybe) this (maybe) fully works?

--- MLP_Final.py
+++ MLP_Final.py
@@ -13,31 +13,40 @@ from tqdm import tqdm
 
 class MLP:
 
-    def __init__(self, load=False, weights="none", biases="none", layer_sizes="auto", activation="relu"):
-        self.version = "1.6"
+    def __init__(self, load=False, weights='none', biases='none', layer_sizes='auto', activation='relu'):
+        self.version = '1.6'
         self.load = load
         self.weights, self.biases = self.set_parameters(weights, biases)
-        if layer_sizes == "auto":
+        if layer_sizes == 'auto':
             self.layer_sizes = [784, 100, 10]
         else:
             self.layer_sizes = layer_sizes
         self.activation = self._get_activation(activation)
+        self.X = None
+        self.Y = None
+        self.valid_X = None
+        self.valid_Y = None
         self.solver = False
         self.batch_size = None
-        self.train_len = 1
+        self.train_len = 0
+        self.valid_len = 0
         self.loss_reporting = False
         self.eval_batching = False
         self.eval_batch_size = None
         self.eval_interval = None
+        self.train_losses = None
+        self.valid_losses = None
+        self.start_time = None
+        self.end_time = None
 
     def set_parameters(self, weights, biases):
-        if weights == "none":
+        if weights == 'none':
             weights = []
             for i in range(len(self.layer_sizes) - 1):
                 weights.append(self.xavier_initialize(self.layer_sizes[i], self.layer_sizes[i + 1]))
         else:
             weights = weights
-        if biases == "none":
+        if biases == 'none':
             biases = []
             for i in range(len(self.layer_sizes) - 1):
                 biases.append(np.zeros((1, self.layer_sizes[i + 1])))
@@ -50,31 +59,31 @@ class MLP:
         return array
 
     def _get_activation(self, name):
-        if name == "sigmoid":
+        if name == 'sigmoid':
             return {
-                "forward": lambda x: 1 / (1 + np.exp(-x)),
-                "derivative": lambda x: x * (1 - x)
+                'forward': lambda x: 1 / (1 + np.exp(-x)),
+                'derivative': lambda x: x * (1 - x)
             }
-        elif name == "tanh":
+        elif name == 'tanh':
             return {
-                "forward": lambda x: np.tanh(x),
-                "derivative": lambda x: 1 - np.tanh(x) ** 2
+                'forward': lambda x: np.tanh(x),
+                'derivative': lambda x: 1 - np.tanh(x) ** 2
             }
-        elif name == "relu":
+        elif name == 'relu':
             return {
-                "forward": lambda x: np.maximum(0, x),
-                "derivative": lambda x: np.where(x > 0, 1, 0)
+                'forward': lambda x: np.maximum(0, x),
+                'derivative': lambda x: np.where(x > 0, 1, 0)
             }
-        elif name == "leaky relu":
+        elif name == 'leaky relu':
             return {
-                "forward": lambda x: np.maximum(0.1 * x, x),
-                "derivative": lambda x: np.where(x > 0, 1, 0.1)
+                'forward': lambda x: np.maximum(0.1 * x, x),
+                'derivative': lambda x: np.where(x > 0, 1, 0.1)
             }
         else:
             raise ValueError(f"'{name}' is an invalid activation function")
 
     def _get_solver(self, name):
-        if name == "sgd":
+        if name == 'sgd':
             def update(nodes, Y, weights, biases, learning_rate, alpha, momentum):
                 d_weights = []
                 d_biases = []
@@ -95,8 +104,8 @@ class MLP:
 
                 return weights, biases
 
-            return {"update": update}
-        elif name == "mini-batch":
+            return {'update': update}
+        elif name == 'mini-batch':
             def update(nodes, Y, weights, biases, learning_rate, alpha, momentum):
                 d_weights = []
                 d_biases = []
@@ -117,50 +126,98 @@ class MLP:
 
                 return weights, biases
 
-            return {"update": update}
+            return {'update': update}
         else:
             raise ValueError(f"'{name}' is an invalid solving method")
 
     def forward(self, inputs):
         nodes = [inputs]
         for layer in range(len(self.layer_sizes) - 1):
-            node_layer = self.activation["forward"](np.matmul(nodes[-1], self.weights[layer]) + self.biases[layer])
+            node_layer = self.activation['forward'](np.matmul(nodes[-1], self.weights[layer]) + self.biases[layer])
             nodes.append(node_layer)
         return nodes
 
-    def loss(self, ):
+    def loss(self, expected, predicted):
+        loss = np.sum(np.subtract(expected, predicted) ** 2)
+        return loss
 
     def predict(self, inputs):
         predicted = np.nanargmax(self.forward(inputs)[-1])
         return predicted
 
-    def fit(self, X, Y, solver="mini-batch", alpha=0.1, batch_size="auto", learning_rate=0.001, max_iter=200, momentum=0.9):
+    def set_valid(self, valid_X, valid_Y):
+        self.valid_X = valid_X
+        self.valid_Y = valid_Y
+        self.valid_len = len(self.valid_X)
+
+    def fit(self, X, Y, solver='mini-batch', alpha=0.1, batch_size='auto', learning_rate=0.001, max_iter=2000, momentum=0.9):
         self.solver = self._get_solver(solver)
-        self.train_len = len(X)
-        if batch_size == "auto":
-            self.batch_size = min(20, len(X))
+        self.X = X
+        self.Y = Y
+        self.train_len = len(self.X)
+        if batch_size == 'auto':
+            self.batch_size = min(20, len(self.X))
         elif (not isinstance(batch_size, int)) or batch_size <= 1:
             raise ValueError(f"'{batch_size}' isn't a valid batch size")
         else:
             self.batch_size = batch_size
-        train_len = len(X)
+        if self.eval_batch_size == 'auto':
+            self.eval_batch_size = min(20, len(self.X))
+        train_losses = []
+        valid_losses = []
+        self.start_time = time.time()
         for epoch in tqdm(range(max_iter), ncols=100, desc='fitting'):
-            if solver == "mini-batch":
-                tc = random.randint(self.batch_size, train_len)
+            if solver == 'mini-batch':
+                tc = random.randint(self.batch_size, self.train_len)
                 inputs = array_X[tc - self.batch_size:tc]
                 expected = array_Y[tc - self.batch_size:tc]
-            elif solver == "sgd":
-                tc = random.randint(0, train_len - 1)
+            elif solver == 'sgd':
+                tc = random.randint(0, self.train_len - 1)
                 inputs = X[tc]
                 expected = Y[tc]
             nodes = self.forward(inputs)
-            self.weights, self.biases = self.solver["update"](nodes, expected, self.weights, self.biases, learning_rate, alpha, momentum)
+            self.weights, self.biases = self.solver['update'](nodes, expected, self.weights, self.biases, learning_rate, alpha, momentum)
+            if self.loss_reporting and epoch % self.eval_interval == 0:
+                tc = random.randint(self.eval_batch_size, self.train_len)
+                valid_tc = random.randint(self.eval_batch_size, self.valid_len)
+                train_pred = self.forward(self.X[tc - self.eval_batch_size:tc])[-1]
+                valid_pred = self.forward(self.valid_X[valid_tc - self.eval_batch_size:valid_tc])[-1]
+                train_loss = self.loss(train_pred, self.Y[tc - self.eval_batch_size:tc])
+                valid_loss = self.loss(valid_pred, self.valid_Y[valid_tc - self.eval_batch_size:valid_tc])
+                train_losses.append(train_loss)
+                valid_losses.append(valid_loss)
+        self.end_time = time.time()
+
+        self.train_losses = train_losses
+        self.valid_losses = valid_losses
+
+    def results(self):
+        loss = np.sum(np.subtract(self.Y, self.forward(X)[-1]) ** 2) / self.train_len
+        val_loss = np.sum(np.subtract(self.valid_Y, self.forward(self.valid_X)[-1]) ** 2) / self.valid_len
+        accu = 0
+        for i in tqdm(range(self.train_len), ncols=100, desc='calculating train accu'):
+            predicted = self.forward(self.X[i])[-1]
+            expected = self.Y[i]
+            if np.nanargmax(predicted) == np.nanargmax(expected):
+                accu += 1
+        accu /= self.train_len
+        val_accu = 0
+        for i in tqdm(range(self.valid_len), ncols=100, desc='calculating val accu'):
+            predicted = self.forward(self.valid_X[i],)[-1]
+            expected = self.valid_Y[i]
+            if np.nanargmax(predicted) == np.nanargmax(expected):
+                val_accu += 1
+        val_accu /= self.valid_len
+        elapsed_time = self.end_time - self.start_time
+
+        return loss, val_loss, accu, val_accu, elapsed_time
+
 
     def configure_reporting(self, loss_reporting=False, eval_batching=True, eval_batch_size="auto", eval_interval=10):
         self.loss_reporting = loss_reporting
         self.eval_batching = eval_batching
-        if eval_batch_size == "auto":
-            self.eval_batch_size = min(5, self.train_len)
+        if eval_batch_size == 'auto':
+            self.eval_batch_size = 'auto'
         elif (not isinstance(eval_batch_size, int)) or eval_batch_size <= 0:
             raise ValueError(f"'{eval_batch_size}' isn't a valid evaluation batch size")
         else:
@@ -213,5 +270,6 @@ array_X_test, array_Y_test = np.array(X_test), np.array(Y_test)
 # class testing
 neural_net = MLP(weights=keras_weights, biases=keras_biases, layer_sizes=[784, 16, 16, 10], activation="leaky relu")
 neural_net.fit(X, Y, solver="mini-batch")
+print(neural_net.results)
 print(neural_net.forward(test_input)[-1])
 print(neural_net.predict(test_input))
---
+++ testgrounds_NN/test13.py
@@ -0,0 +1,13 @@
+import random
+#
+# test_list = [1, 2, 3, 4, 5, 6, 7]
+# back_length = 5
+#
+# print
+#
+# for i in range(25):
+#     tc = random.randint(0, len(test_list))
+#     print(tc)
+#     print(test_list[tc - back_length:tc])
+
+print(random.randint(5, 0))
