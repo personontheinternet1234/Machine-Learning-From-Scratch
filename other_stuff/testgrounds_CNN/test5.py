7122d07116f13888549053b3c053cd973e4d98da
githubCHM <143050709+githubCHM@users.noreply.github.com>
2024-06-13 10:38:01 -1000

    added new test images

--- other_stuff/testgrounds_CNN/test5.py
+++ other_stuff/testgrounds_CNN/test5.py
@@ -11,7 +11,7 @@ k4 = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])  # bottom
 kb = np.array([[1, 4, 6, 4, 1], [4, 16, 24, 16, 4], [6, 24, 36, 24, 6], [4, 16, 24, 16, 4], [1, 4, 6, 4, 1]]) / 256  # blur
 
 
-img = Image.open(f'test_images/25-256245_m copy.jpg')
+img = Image.open(f'test_images/_DWS6234-Edit-Edit.jpg')
 gray_img = img.convert('L')
 test_image = np.array(gray_img)
 test_image = test_image.astype('uint8')
@@ -23,7 +23,7 @@ plt.show()
 plt.close()
 
 for i in range(14):
-    convolved_image = convolve2d(test_image, k1, mode='same')
+    convolved_image = convolve2d(test_image, k3, mode='same')
     test_image = convolved_image
     plt.imshow(convolved_image, cmap='gray')
     plt.title(str(i + 1))
