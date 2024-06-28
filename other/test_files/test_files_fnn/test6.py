7fc7096060d9285f2b4c757fecdece5c586ba38e
githubCHM <143050709+githubCHM@users.noreply.github.com>
2024-06-28 05:02:55 -1000

    Update test6.py

--- other/test_files/test_files_fnn/test6.py
+++ other/test_files/test_files_fnn/test6.py
@@ -1,4 +1,6 @@
 import time
+import numpy as np
+arr = np.random.randn(10, 10)
 
 l1 = []
 l2 = []
@@ -6,17 +8,17 @@ l3 = []
 l4 = []
 one = time.time()
 for i in range(100000):
-    l1.append(i)
+    l1.append(arr * arr)
 two = time.time()
 for i in range(100000):
-    l2.insert(0, i)
+    l2.insert(0, arr * arr)
 three = time.time()
 for i in range(100000):
-    j = i * 3
+    j = arr * arr
     l3.append(j)
 four = time.time()
 for i in range(100000):
-    l4.append(i * 3)
+    l4.append(arr * arr)
 five = time.time()
 
 print(two - one)
