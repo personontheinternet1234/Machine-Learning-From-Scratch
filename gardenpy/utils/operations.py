fixed some gradient internals
the gradient tracking is fine, but the forward tracking is messed up and needs to be changed

gardenpy/utils/operations.py
@@ -53,12 +53,15 @@ def nabla(grad, rspc):
             other = other.to_np()
         result = Tensor(back[operation_type](rspc.to_np(), other))
         result._type = 'grad'
-        result._tracker['operations'] += f'd_{operation_type}'
+        result._tracker['operations'].append(f'd_{operation_type}')
         result._tracker['relations'] += [grad, rspc]
         return result
     else:
         raise ValueError('no relation')
 
+
+def test_tracker(itm1, itm2):
+    print(repr(itm1))
 
 def chain(grad_loc, grad_glob):
     if not isinstance(grad_loc, Tensor):
@@ -66,16 +69,16 @@ def chain(grad_loc, grad_glob):
     if not isinstance(grad_glob, Tensor):
         raise TypeError('glob not tensor')
 
-    if not (grad_loc._type == 'grad' and grad_glob._type == 'grad'):
+    if not (grad_loc._type in ('grad', 'grad_chain') and grad_glob._type in ('grad', 'grad_chain')):
         raise TypeError('not gradients')
 
     glob_conn = grad_glob._tracker['relations'][0]
     loc_conn = grad_loc._tracker['relations'][-1]
     if glob_conn == loc_conn:
         result = Tensor(np.dot(grad_loc.to_np(), grad_glob.to_np()))  # todo: check this math
-        result._type = 'grad'
+        result._type = 'grad_chain'
         result._tracker['relations'] = grad_loc._tracker['relations'] + grad_glob._tracker['relations']
-        result._tracker['operations'] = grad_loc._tracker['operations'] + grad_glob._tracker['operations']
+        result._tracker['operations'] = grad_loc._tracker['operations'] + (grad_glob._tracker['operations'])
         return result
     else:
         raise TypeError('no relation')