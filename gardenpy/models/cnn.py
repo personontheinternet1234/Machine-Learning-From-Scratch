a96a454bd5d8cf5bb63c6cf390dc1a5c9be5f5e1
githubCHM <143050709+githubCHM@users.noreply.github.com>
2024-07-28 23:48:54 -0500

    Update cnn.py
    unfortunate final desc for cnn (at least for now)

--- gardenpy/models/cnn.py
+++ gardenpy/models/cnn.py
@@ -9,4 +9,10 @@ Refer to 'todo' for in-depth documentation on this model.
 
 
 class CNN:
+    r"""
+    This model will most likely remain incomplete until the DNN is fully completed.
+    This is due to the complexity required to code a CNN from scratch accompanied by a lack of time.
+    If in the future, this project is still worked on, this model will see completion.
+    However, most likely, only cnn_torch.py will be completed.
+    """
     ...
