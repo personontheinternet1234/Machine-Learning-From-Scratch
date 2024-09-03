140fece3b9732b5a7612570c7cc22b53c61cef90
githubCHM <143050709+githubCHM@users.noreply.github.com>
2024-09-03 12:11:37 -1000

    added a forward pass script for website implementation
    under other/application_test. There are 2 files for different versions of the application.

---
+++ other/application_test/preload_forward.py
@@ -0,0 +1,38 @@
+# run without any gardenpy imports
+
+def main(a0=None):
+    import ast
+    import os
+
+    import numpy as np
+
+    def load_params(root):
+        weights = []
+        biases = []
+        with open(os.path.join(root, 'weights.txt'), 'r') as f:
+            for line in f:
+                weights.append(np.array(ast.literal_eval(line)))
+        with open(os.path.join(root, 'biases.txt'), 'r') as f:
+            for line in f:
+                biases.append(np.array(ast.literal_eval(line)))
+        return weights, biases
+
+    def lrelu(x):
+        return np.max(0.1 * x, x)
+
+    def softmax(x):
+        return np.exp(x) / np.sum(np.exp(x))
+
+    w, b = load_params(os.path.dirname(__file__))
+
+    a1 = lrelu(a0 @ w[0] + b[0])
+    a2 = lrelu(a1 @ w[1] + b[1])
+    a3 = lrelu(a2 @ w[2] + b[2])
+
+    return {'norm': a3, 'pred': np.argmax(a3), 'prob': softmax(a3)}
+
+
+if __name__ == '__main__':
+    # todo: set input image here
+    image = ...
+    main(image)
---
+++ other/application_test/preload_forward_imports.py
@@ -0,0 +1,36 @@
+# run with gardenpy imports
+
+def main(a0=None):
+    import ast
+    import os
+
+    import numpy as np
+    from gardenpy import Activators
+
+    def load_params(root):
+        weights = []
+        biases = []
+        with open(os.path.join(root, 'weights.txt'), 'r') as f:
+            for line in f:
+                weights.append(np.array(ast.literal_eval(line)))
+        with open(os.path.join(root, 'biases.txt'), 'r') as f:
+            for line in f:
+                biases.append(np.array(ast.literal_eval(line)))
+        return weights, biases
+
+    w, b = load_params(os.path.dirname(__file__))
+
+    act = Activators('lrelu', beta=0.1).activate
+    pred = Activators('softmax').activate
+
+    a1 = act(a0 @ w[0] + b[0])
+    a2 = act(a1 @ w[1] + b[1])
+    a3 = act(a2 @ w[2] + b[2])
+
+    return {'norm': a3, 'pred': np.argmax(a3), 'prob': pred(a3)}
+
+
+if __name__ == '__main__':
+    # todo: set input image here
+    image = ...
+    main(image)
