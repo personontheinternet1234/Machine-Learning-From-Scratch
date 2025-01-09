ab9c6971a147d081b8fbd1fedd38e5dc0b194e31
chostmadsen25 <143050709+githubCHM@users.noreply.github.com>
2025-01-09 01:45:07 -1000

    Update test75.py
    reverted to what works briefly, and added loss printing methods. Will debug optimizers in another test file.

--- other/tests/test75.py
+++ other/tests/test75.py
@@ -11,13 +11,12 @@ x = tensor([[1, 0.5]])
 w = init(1, 2)
 y = tensor([[0.5, 1]])
 
-for i in range(10000):
+for i in range(10_000):
     yhat = x * w
     loss = criterion(yhat, y)
     grad_w = nabla(w, loss)
     itm_id = w.id
-    w = optim(w, grad_w)
-    # w = w - grad_w * np.array([[0.01]])
-    # Tensor.instance_replace(itm_id, w)
-    print(yhat)
+    w = w - grad_w * np.array([[0.01]])
+    Tensor.instance_replace(itm_id, w)
+    print(f"Loss: {str(loss)[2:-2]}")
     Tensor.zero_grad(x, w, y)
